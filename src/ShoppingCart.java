4dfc25825e5624ccbcc32d73236d69d394c877fd
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-11-28 22:23:36 +0530

    add User and Shopping Cart Class

---
+++ src/ShoppingCart.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<Product> products;
+
+    public ShoppingCart() {
+        products=new ArrayList<>();
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void removeProduct(Product product){
+        products.remove(product);
+    }
+
+    public double calculateTotalCost() {
+        double totalCost = 0.0;
+        for (Product product : products) {
+            totalCost += product.getPrice();
+        }
+        return totalCost;
+    }
+}
