3559287ccad26f18f549655b160c7b4a6d71b769
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-12-23 08:02:13 +0530

    Update NoEditableTableModel.java

--- src/gui/def/NoEditableTableModel.java
+++ src/gui/def/NoEditableTableModel.java
@@ -1,4 +1,19 @@
 package gui.def;
-public class NoEditableTableModel {
-    
+
+import javax.swing.table.DefaultTableModel;
+import java.util.Vector;
+
+public class NoEditableTableModel extends DefaultTableModel {
+
+    public NoEditableTableModel() {
+    }
+
+    public NoEditableTableModel(Object[] columnNames, int rowCount) {
+        super(columnNames, rowCount);
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return false;
+    }
 }
