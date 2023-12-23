d2e7fc84a996508fab25eddfac3cec0c9c8ea5df
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-12-23 08:03:14 +0530

    Create Test.java

---
+++ src/cli/Test.java
@@ -0,0 +1,8 @@
+package cli;
+
+public class Test {
+    public static void main(String[] args) {
+        WestminsterShoppingManager manager=new WestminsterShoppingManager();
+        manager.displayMenu();
+    }
+}
